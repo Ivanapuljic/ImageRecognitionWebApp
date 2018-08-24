13831b453f3b3711687122111029367b72a6848c
DESKTOP-HMFH6EA\ivana <ivana.pristavnik@gmail.com>
2018-08-24 22:18:19 +0200

    Python classification script, trained model and test data

---
+++ Classification/result.py
@@ -0,0 +1,33 @@
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+import numpy as np
+from keras.preprocessing import image
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+# Step 2 - Pooling
+model.add(MaxPooling2D(pool_size = (2, 2)))
+# Adding a second convolutional layer
+model.add(Conv2D(32, (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+# Step 3 - Flattening
+model.add(Flatten())
+# Step 4 - Full connection
+model.add(Dense(units = 128, activation = 'relu'))
+model.add(Dense(units = 1, activation = 'sigmoid'))
+# Compiling the CNN
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+model.load_weights('models/CNN.h5')
+
+test_image = image.load_img('picture.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = model.predict(test_image)
+
+if result[0][0] == 1:
+    prediction = 'daisy'
+else: prediction = 'rose'
+print(prediction)
